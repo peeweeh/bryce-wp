2820b9bee8ce77033243952acf009d569bb5c72f
Paul Hidalgo <me@paulhidalgo.net>
2017-09-13 16:02:41 +0800

    Modified

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 echo "create database wordpress" | mysql -u root
 rm -rf /etc/apache2/sites-available/000-default.conf
 cp ./000-default.conf /etc/apache2/sites-available/000-default.conf
-mysql wordpress < bryce_blog_gcp.sql 
+mysql wordpress < ./bryce_blog_gcp.sql 
