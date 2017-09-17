ce65fa570d52cfacd61bd8a4e5f7c096c4738c3c
Paul Hidalgo <me@paulhidalgo.net>
2017-09-18 02:55:26 +0800

    New WP Config Files

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ echo "create database wordpress" | mysql -u root
 rm -rf /etc/apache2/sites-available/000-default.conf
 cp ./000-default.conf /etc/apache2/sites-available/000-default.conf
 mysql wordpress < ./bryce_blog_gcp.sql 
+rm -rf /var/www/html/wp-config.php
+mv /var/www/html/wp-config-gcp.php /var/www/html/wp-config.php
